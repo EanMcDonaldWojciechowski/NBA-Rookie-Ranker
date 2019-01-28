b9a16a0d84588d0ab594fa480690aec066815646
Ean McDonald Wojciechowski <ean@Eans-MacBook-Pro.local>
2019-01-28 14:11:56 -0500

    Data for current rookies has been called and stored in a dataframe

--- main.py
+++ main.py
@@ -4,18 +4,12 @@ from sklearn import linear_model
 import pandas as pd
 import time
 
-
-
 player_info = commonallplayers.CommonAllPlayers()
-
 player_json = player_info.get_json()
-
 parsed_player = json.loads(player_json)
-
 
 playerdata = parsed_player.get("resultSets")[0]['rowSet']
 playerDataHeader = parsed_player.get("resultSets")[0]['headers']
-print(playerDataHeader)
 
 # SET COLUMNS LATER
 listOfRookieID = []
@@ -25,41 +19,38 @@ for playerInfo in playerdata:
     # playerInfo[4] is year started playing
     if playerInfo[4] == '2018':
         # set key in dict equal to player ID, value to rest of player info
-        listOfRookieID.append(playerInfo[0])
-        
+        listOfRookieID.append(playerInfo[0])    
 
 
 listOfRookieStatsObj = []
 player_stat_json = []
 player_stat_parsed = []
-listOfRookieStats = []
+RookieStats2018 = []
 listOfRookieStatHeader = []
 
+
 for idx in enumerate(listOfRookieID):
-    print(listOfRookieID[idx[0]])
-    listOfRookieStatsObj.append(playercareerstats.PlayerCareerStats(listOfRookieID[idx[0]]))
+    listOfRookieStatsObj.append(playercareerstats.PlayerCareerStats(listOfRookieID[idx[0]], per_mode36="PerGame"))
     time.sleep(1)
     player_stat_json.append(listOfRookieStatsObj[idx[0]].get_json())
     player_stat_parsed.append(json.loads(player_stat_json[idx[0]]))
-    #print(player_stat_parsed[idx[0]])
-    listOfRookieStats.append(player_stat_parsed[idx[0]].get("resultSets")[0]['rowSet'])
-    print(listOfRookieStats[idx[0]])
+    if (len(player_stat_parsed[idx[0]].get("resultSets")[0]['rowSet']) < 1):
+        RookieStats2018.append([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+    elif (len(player_stat_parsed[idx[0]].get("resultSets")[0]['rowSet']) >= 1):
+        RookieStats2018.append(player_stat_parsed[idx[0]].get("resultSets")[0]['rowSet'][0])
+    print(RookieStats2018[idx[0]])
 
 
-listOfRookieStatHeader.append(player_stat_parsed[idx[0]].get("resultSets")[0]['headers'])
+listOfRookieStatHeader = player_stat_parsed[idx[0]].get("resultSets")[0]['headers']
 print(listOfRookieStatHeader)
+df_rookiesTestData = pd.DataFrame(data=RookieStats2018, columns=listOfRookieStatHeader) 
+df_rookiesTestData = df_rookiesTestData.drop(['SEASON_ID', 'LEAGUE_ID', 'TEAM_ID', 'TEAM_ABBREVIATION', 'PLAYER_AGE', 
+'GS', 'FGA', 'FG3A', 'FTA', 'OREB', 'DREB', 'TOV', 'PF'], axis=1)
 
-print(listOfRookieStats[0])
-print(listOfRookieStatHeader)
-#df_rookies = pd.DataFrame(listOfRookieID, columns=['ID'])
-df_rookies = pd.DataFrame(listOfRookieStats, columns=listOfRookieStatHeader)
-#print(df_rookies)
+print(df_rookiesTestData)
+
 
 #################################
 #df_rookies.to_csv("output.csv", encoding='utf-8')
 #df = df_rookies.read_csv("output.csv")
 
-
-#rookieStatsForReg = {}
-#for id, info  in rookieDict.items():
-# rookieStatsForReg[rookieStatsForReg[]]
