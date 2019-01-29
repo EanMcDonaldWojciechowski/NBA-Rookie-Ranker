205fb29e481e52d368d3b82289b8bf3d11648323
EanMcDonaldWojciechowski <eanmcdwoj@gmail.com>
2019-01-29 14:51:25 -0500

    split training and test data and storing in csv

--- main.py
+++ main.py
@@ -4,62 +4,8 @@ from sklearn import linear_model
 import pandas as pd
 import time
 
-player_info = commonallplayers.CommonAllPlayers()
-player_json = player_info.get_json()
-parsed_player = json.loads(player_json)
-
-playerdata = parsed_player.get("resultSets")[0]['rowSet']
-playerDataHeader = parsed_player.get("resultSets")[0]['headers']
-
-# SET COLUMNS LATER
-listOfRookieID = []
-TrainingDataRookieIDs = []
-
-#playerStats = []
-for playerInfo in playerdata:
-    # playerInfo[4] is year started playing
-    if playerInfo[4] == '2018':
-        # set key in dict equal to player ID, value to rest of player info
-        listOfRookieID.append(playerInfo[0])        
-    elif playerInfo[4] != '2018':
-        TrainingDataRookieIDs.append(playerInfo[0])
-
-print(len(TrainingDataRookieIDs))
-listOfRookieStatsObj = []
-player_stat_json = []
-player_stat_parsed = []
-
-TrainingRookieStatsObj = []
-TrainingPlayer_stat_json = []
-TrainingPlayer_stat_parsed = []
-
-RookieStats2018 = []
-TrainingRookieStats = []
-listOfRookieStatHeader = []
-
-
-for idx in enumerate(listOfRookieID):
-    listOfRookieStatsObj.append(playercareerstats.PlayerCareerStats(listOfRookieID[idx[0]], per_mode36="PerGame"))
-    time.sleep(1)
-    player_stat_json.append(listOfRookieStatsObj[idx[0]].get_json())
-    player_stat_parsed.append(json.loads(player_stat_json[idx[0]]))
-    if (len(player_stat_parsed[idx[0]].get("resultSets")[0]['rowSet']) < 1):
-        RookieStats2018.append([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
-    elif (len(player_stat_parsed[idx[0]].get("resultSets")[0]['rowSet']) >= 1):
-        RookieStats2018.append(player_stat_parsed[idx[0]].get("resultSets")[0]['rowSet'][0])
-    print(RookieStats2018[idx[0]])
-
-
-listOfRookieStatHeader = player_stat_parsed[idx[0]].get("resultSets")[0]['headers']
-print(listOfRookieStatHeader)
-df_rookiesTestData = pd.DataFrame(data=RookieStats2018, columns=listOfRookieStatHeader) 
-df_rookiesTestData = df_rookiesTestData.drop(['SEASON_ID', 'LEAGUE_ID', 'TEAM_ID', 'TEAM_ABBREVIATION', 'PLAYER_AGE', 
-'GS', 'FGA', 'FG3A', 'FTA', 'OREB', 'DREB', 'TOV', 'PF'], axis=1)
-
-print(df_rookiesTestData)
-
-
-#################################
-#df_rookies.to_csv("output.csv", encoding='utf-8')
 #df = df_rookies.read_csv("output.csv")
+
+
+
 
---
+++ trainingData.py
@@ -0,0 +1,51 @@
+from nba_api.stats.endpoints import commonallplayers, playercareerstats
+import json
+import pandas as pd
+import time
+
+player_info = commonallplayers.CommonAllPlayers()
+player_json = player_info.get_json()
+parsed_player = json.loads(player_json)
+
+playerdata = parsed_player.get("resultSets")[0]['rowSet']
+playerDataHeader = parsed_player.get("resultSets")[0]['headers']
+
+TrainingDataIDs = []
+
+#playerStats = []
+for playerInfo in playerdata:
+    # playerInfo[4] is year started playing    
+    if playerInfo[4] != '2018':
+        TrainingDataIDs.append(playerInfo[0])
+
+       
+TrainingStatsObj = []
+TrainingPlayer_stat_json = []
+TrainingPlayer_stat_parsed = []
+
+TrainingStats = []
+
+# parsing and collecting training data
+for idx in enumerate(TrainingDataIDs):
+    TrainingStatsObj.append(playercareerstats.PlayerCareerStats(TrainingDataIDs[idx[0]], per_mode36="PerGame"))
+    time.sleep(1)
+    TrainingPlayer_stat_json.append(TrainingStatsObj[idx[0]].get_json())
+    TrainingPlayer_stat_parsed.append(json.loads(TrainingPlayer_stat_json[idx[0]]))
+    if (len(TrainingPlayer_stat_parsed[idx[0]].get("resultSets")[0]['rowSet']) < 1):
+        TrainingStats.append([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+    elif (len(TrainingPlayer_stat_parsed[idx[0]].get("resultSets")[0]['rowSet']) >= 1):
+        TrainingStats.append(TrainingPlayer_stat_parsed[idx[0]].get("resultSets")[0]['rowSet'][0])
+    print(TrainingStats[idx[0]])
+    if idx[0] > 300:
+        break
+    print(TrainingDataIDs[idx[0]])    
+
+
+trainingStatHeader = TrainingPlayer_stat_parsed[idx[0]].get("resultSets")[0]['headers']
+
+df_TrainingData = pd.DataFrame(data=TrainingStats, columns=trainingStatHeader) 
+df_TrainingData = df_TrainingData.drop(['SEASON_ID', 'LEAGUE_ID', 'TEAM_ID', 'TEAM_ABBREVIATION', 'PLAYER_AGE', 
+'GS', 'FGA', 'FG3A', 'FTA', 'OREB', 'DREB', 'TOV', 'PF'], axis=1)
+print(df_TrainingData)
+
+df_TrainingData.to_csv("TrainingData.csv", encoding='utf-8')
